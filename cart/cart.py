218448e45aaf942749c11afad0f7d0c95d0b8aaa
Jack <jb.brown@mail.utoronto.ca>
2019-01-19 22:21:20 -0500

    Updated documentation

--- cart/cart.py
+++ cart/cart.py
@@ -2,7 +2,11 @@ from inventory.models import Product
 from typing import Dict, NamedTuple
 from decimal import Decimal
 
+
+
 class CartEntry:
+    """ Class representing a single product entry in a shopping cart.
+    """
 
     product: 'Product'
     qty: int
@@ -18,6 +22,9 @@ class CartEntry:
         return " : ".join((str(self.qty), str(self.product)))
 
     def is_valid(self) -> bool:
+        """
+        verify whether a given entry is valid, i.e purchasable
+        """
         return self.qty <= self.product.inventory_count
 
     def sub_total(self) -> Decimal:
@@ -25,6 +32,20 @@ class CartEntry:
 
 
 class Cart:
+    """ Class representing a shopping cart.
+
+        
+        Attributes
+        ----------
+
+        entries: Dict[int, CartEntry]
+            the products contained in the cart, keyed
+            by the product id of the product in CartEntry
+
+        total: Decimal
+            the total cost of all products in the shopping cart
+
+    """
 
     entries: Dict[int, 'CartEntry']
     total: Decimal
@@ -45,6 +66,24 @@ class Cart:
 
     def __edit_entry(self, entry: 'CartEntry', d_qty: int) -> int:
 
+        """
+        if the sum of d_qty and entry.qty is <= 0, 
+        remove entry from self.entries.
+
+        otherwise, set entry.qty to the maximum of the sum
+        or entry.product.inventory_count.
+
+        return the new quantity of the given entry
+
+        Arguments
+        ---------
+
+        entry: an entry in this cart's entries dict.
+
+        d_qty: an integer
+
+        """
+
         old_sub_total = entry.sub_total()
         new_qty = max(0, entry.qty + d_qty)
 
@@ -61,7 +100,21 @@ class Cart:
 
 
     def add_product(self, product: Product, qty: int=1) -> int:
+        """
+        add at the minimum of qty, product.inventory_count of 
+        product to cart.
 
+        return the number of the given product contained in 
+        the cart after adding.
+
+        Arguments
+        ---------
+
+        product: a product
+
+        qty: a postive integer
+
+        """
         if not product.id in self.entries:
             entry = CartEntry(product, 0)
             self.entries[product.id] = entry
@@ -73,21 +126,46 @@ class Cart:
 
     
     def remove_product(self, product: Product, qty: int=1) -> int:
+        """
+        remove the maximum of qty, and the current number of product in 
+        this cart, to cart.
+
+        return the number of the given product contained in the cart after
+        removing
+
+        Arguments
+        ---------
+
+        product: a product
+
+        qty: a positive integer
+        """
         return self.add_product(product, -qty)
 
     
-    def validate(self) -> bool:
+    def validate(self) -> Dict[int, 'CartEntry']:
+        """
+        check each entry in the cart for validity,
+        i.e. whether it is purchasable or not.
+
+        return a dict containg every invalid entry
+        """
         invalid = {}
 
         for key, entry in self.entries.items():
             if not entry.is_valid():
                 invalid[key] = entry
 
-        return len(invalid) == 0
+        return invalid
 
     def complete_cart(self) -> bool:
+        """
+        if cart is valid, purchase each item and return True.
 
-        if not self.validate():
+        otherwise, do nothing and return False.
+        """
+
+        if not len(self.validate()) == 0:
             return False
 
         for prod_id in self.entries:
--- cart/search.py
+++ cart/search.py
@@ -6,7 +6,29 @@ from decimal import *
 from typing import Tuple
 
 class ProductSearch:
+    """
+    Represents a Product search by filter parameters. All parameters
+    are optional, a search with no parameters will return every
+    Product in inventory.
 
+    uses Django filters to implement Product filtering.
+
+    Attributes
+    ----------
+
+    min_qty: int
+        positive integer, defaults to 0. minimum inventory count 
+        required for product to appear in search.
+
+    title: str
+        part or whole of product title. defaults to empty string,
+        which will match any product.
+        will be truncated if longer than MAX_TITLE_LEN
+
+    price_range: Tuple[Decimal, Decimal]
+        inclusive price interval for product search.
+    
+    """
     def __init__(self,
             min_qty: int=0,
             title: str="",
@@ -22,10 +44,15 @@ class ProductSearch:
 
     def query(self) -> QuerySet:
 
+        """
+        Perform search with current parameters.
+
+        return a QuerySet with every matching Product. 
+        """
+
         match = Product.objects.filter(inventory_count__gte=self.min_qty)
 
         if self.title is not None:
-
             match = match.filter(title__contains=self.title)
 
         if self.price_range is not None:
